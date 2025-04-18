1ed0ddcf5a3279ef6df38731f08b12776450255c
Anubhaba Samanta <anubhaba17@gmail.com>
2025-04-18 19:33:32 +0530

    first commit

---
+++ src/main/java/net/engineeringdigest/journalapp/entity/JournalEntry.java
@@ -0,0 +1,18 @@
+package net.engineeringdigest.journalapp.entity;
+
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "journal_entries")
+@Data
+public class JournalEntry {
+    @Id
+    private ObjectId id;
+    private String title;
+    private String content;
+    private LocalDateTime date;
+}
---
+++ src/test/java/net/engineeringdigest/journalapp/service/UserServiceTests.java
@@ -0,0 +1,48 @@
+package net.engineeringdigest.journalapp.service;
+
+import net.engineeringdigest.journalapp.repository.UserRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class UserServiceTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeAll
+    static void setUp(){
+
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @ValueSource(strings ={
+            "ram",
+            "Shyam"
+    })
+    public void testFindByUserName() {
+        assertNotNull(userRepository.findByUserName("ram"));
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,2",
+            "2,10,12"
+    })
+    public void test(int a, int b, int expected){
+        assertEquals(expected, a+b);
+    }
+}
